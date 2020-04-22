1babc59050d6911f06ef49bb5f1366b64db8ddac
Hector Fugihara Kroes <hector.kroes@outlook.com>
2020-04-22 18:58:59 -0300

    Version 2.2
    -Table error corrected
    -Reopen graph function added
    -Minor visual improvements made
    -References updated

--- LBplot.py
+++ LBplot.py
@@ -16,14 +16,12 @@ def menu():
 def sep():
 	print('----------------------------------------------------------')
 
-V0 = []
-S = []
-
-oV0 = []
-oS = []
-
 st='start'
 while st == 'start':
+	V0 = []
+	S = []
+	oV0 = []
+	oS = []
 	cls()
 	print('''What would you like to access?
 	1-Execute program
@@ -39,13 +37,13 @@ while st == 'start':
 	1- V0 and [S]
 	2- 1/V0 and 1/[S]''')
 		k = input()
-		print('''Which are the the unities your data use?
+		print('''\nWhich are the the unities your data use?
 V0:''')
 		u = input()
 		uV = u.split('/')
 		print('[S]:')
 		v = input()
-		print('''What input format do you prefer?
+		print('''\nWhat input format do you prefer?
 	1- Separated by commas (x, y, z, h, i, j)
 	2- Individually (x)''')
 		h = input()
@@ -53,7 +51,7 @@ V0:''')
 			continue
 		if k == '1':
 			if h == '1':
-				print('Insert V0 values:')
+				print('\nInsert V0 values:')
 				tmpV0 = (input().replace(' ', '')).split(',')
 				print('Insert [S] values:')
 				tmpS = (input().replace(' ', '')).split(',')
@@ -67,12 +65,12 @@ V0:''')
 					oS.append(ob)
 
 			elif h == '2':
-				print('How many values would you like to insert?')
+				print('\nHow many values would you like to insert?')
 				l = int(input())
 				n=0
 				while n < l:
 					if n == 0:
-						print('Insert V0 values:')
+						print('\nInsert V0 values:')
 						print('V0'+str(n+1)+':')
 						V0.append(float(input()))
 						n+=1
@@ -101,7 +99,7 @@ V0:''')
 					oS.append(ob)
 
 		elif k == '2':
-			n=0
+			n = 0
 			if h == '2':
 				print('How many values would you like to insert?')
 				l = int(input())
@@ -110,22 +108,22 @@ V0:''')
 						print('Insert 1/V0 values:')
 						print('1/V0'+str(n+1)+':')
 						oV0.append(float(input()))
-						n+=1
+						n += 1
 					else:
 						print('1/V0'+str(n+1)+':')
 						oV0.append(float(input()))
-						n+=1
-				m=0
+						n += 1
+				m = 0
 				while m < l:
 					if m == 0:
 						print('Insert 1/[S] values:')
 						print('1/[S]'+str(m+1)+':')
 						oS.append(float(input()))
-						m+=1
+						m += 1
 					else:
 						print('1/[S]'+str(m+1)+':')
 						oS.append(float(input()))
-						m+=1
+						m += 1
 
 			elif h == '1':
 				print('Insert 1/V0 values:')
@@ -142,7 +140,7 @@ V0:''')
 					oS.append(ob)
 
 		x = oS
-		y= oV0
+		y = oV0
 
 		plt.scatter(x, y)
 
@@ -195,33 +193,56 @@ V0:''')
 		sep()
 		print('\n                    LINEAR REGRESSION\n')
 		sep()
-		if std_err>=100:
-			print('| Correlation Coefficient (R)             '+str(round(rv, 9))+'    |\n| Coefficient of Determination (R^2)      '+str(round(rv**2, 9))+'    |\n| Standard error                        '+str(round(std_err, 9))+'    |\n| P-Value                                 '+Pstat(pv)+'    |')
-		elif std_err>=10:
-			print('| Correlation Coefficient (R)             '+str(round(rv, 9))+'    |\n| Coefficient of Determination (R^2)      '+str(round(rv**2, 9))+'    |\n| Standard error                         '+str(round(std_err, 9))+'    |\n| P-Value                                 '+str("{:.5e}".format(pv))+'    |')
-		else:
-			print('| Correlation Coefficient (R)             '+str(round(rv, 9))+'    |\n| Coefficient of Determination (R^2)      '+str(round(rv**2, 9))+'    |\n| Standard error                          '+str(round(std_err, 9))+'    |\n| P-Value                                 '+str("{:.5e}".format(pv))+'    |')
+		print('| Correlation Coefficient (R)             '+str("{:.5e}".format(rv))+'    |\n| Coefficient of Determination (R^2)      '+str("{:.5e}".format(rv**2))+'    |\n| Standard error                          '+str("{:.5e}".format(std_err))+'    |\n| P-Value                                 '+str("{:.5e}".format(pv))+'    |')
 		sep()
 		print('\n                 VARIANCES AND DEVIATIONS\n')
 		sep()
-		print('| Population Variance (σ^2)                  '+str("{:.2e}".format(errorg2))+'    |\n| Population Standard Deviation (σ)          '+str("{:.2e}".format(errorg)) +'    |\n| Sample Variance (S^2)                      '+str("{:.2e}".format(errors2))+'    |\n| Sample Standard Deviation (S)              '+str("{:.2e}".format(errors)) +'    |')
+		print('| Population Variance (σ^2)               '+str("{:.5e}".format(errorg2))+'    |\n| Population Standard Deviation (σ)       '+str("{:.5e}".format(errorg)) +'    |\n| Sample Variance (S^2)                   '+str("{:.5e}".format(errors2))+'    |\n| Sample Standard Deviation (S)           '+str("{:.5e}".format(errors)) +'    |')
 		sep()
 		plt.show()
 
-		menu()
-		me = input()
-		if me == '1':
-			continue
-		if me == '2':
-			break
+		print ('''\nWould you like to return to the main menu?
+		1-Yes, return to main menu
+		2-No, reopen graph
+		3-No, end program''')
+		sm = 'start'
+		while sm == 'start':
+			me = input()
+			if me == '1':
+				sm = 'end'
+				continue
+			elif me == '2':
+				plt.plot(x, mymodel, '-r')
+				plt.plot([(0-int(intercept/slope))], [0],'co', label= 'Vmax = '+str((1/intercept)))
+				plt.plot([0], [intercept],'go', label = 'Km = '+str((slope/intercept)))
+				plt.suptitle('Lineweaver-Burk double reciprocal plot')
+				plt.xlabel('1/[S] '+'(1/'+v+')', color='#298A08')
+				if len(uV) == 1:
+					plt.ylabel('1/V0 '+'(1/'+u+')', color='#01DFD7')
+				elif len(uV) == 2:
+					plt.ylabel('1/V0 '+'('+str(uV[1])+'/'+str(uV[0])+')', color='#01DFD7')
+				plt.legend()
+				plt.grid()
+				plt.show()
+			elif me == '3':
+				st = 'end'
+				break
 
 	if ii=='2':
 		cls()
-		print('''Lineweaver, H., & Burk, D. (1934). The Determination of Enzyme Dissociation Constants. Journal of the American Chemical Society, 56(3), 658–666. doi:10.1021/ja01318a036
+		print('''REFERENCES
 
-Michaelis, L., and Menten, M. L. (1913) Die Kinetik der Invertinwirkung. Biochem. Z. 49, 333–369
+-Lineweaver, H., & Burk, D. (1934). The Determination of Enzyme Dissociation Constants. Journal of the American Chemical Society, 56(3), 658–666. doi:10.1021/ja01318a036
 
-Knight K. (2000), Mathematical Statistics, Chapman and Hall, New York. (proposition 2.11)''')
+-Michaelis, L., and Menten, M. L. (1913) Die Kinetik der Invertinwirkung. Biochem. Z. 49, 333–369
+
+-Evans, M.; Hastings, N.; and Peacock, B. Statistical Distributions, 3rd ed. New York: Wiley, p. 12-14, 2000.
+
+-Weisstein, Eric W. "Hypothesis Testing." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/HypothesisTesting.html 
+
+-Weisstein, Eric W. "Standard Error." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/StandardError.html 
+
+-Weisstein, Eric W. "Correlation Coefficient." From MathWorld--A Wolfram Web Resource. https://mathworld.wolfram.com/CorrelationCoefficient.html ''')
 		menu()
 		me = input()
 		if me == '1':
@@ -258,4 +279,4 @@ Thank you!''')
 		if me == '2':
 			break
 
-print('Program suspended!')
+print('\nProgram suspended!')
